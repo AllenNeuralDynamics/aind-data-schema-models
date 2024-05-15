6b062f202baaf9bf46f6bc70ea1de859d595b5ed
Mae Moninghoff <mae.moninghoff@alleninstitute.org>
2024-05-15 16:44:08 -0400

    adding test script

---
+++ tests/test_specimen_procedure_types.py
@@ -0,0 +1,11 @@
+from aind_data_schema_models.specimen_procedure_types import SpecimenProcedureType
+import unittest
+
+
+class TestSpecimenProcedureTypes(unittest.TestCase):
+    """Tests methods in SpecimenProcedureType class"""
+
+    def test_class_construction(self):
+        """Tests enum can be instantiated via string"""
+
+        self.assertEqual(SpecimenProcedureType.DELIPIDATION, SpecimenProcedureType("Delipidation"))
