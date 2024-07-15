6fc56ac47ad0e0127aafe97cfa00c037f1de9a4c
dyf <david.feng@alleninstitute.org>
2024-07-15 13:20:36 -0700

    adding registries

--- src/aind_data_schema_models/registry.py
+++ src/aind_data_schema_models/registry.py
@@ -3,66 +3,23 @@
 from typing import Literal
 
 from pydantic import ConfigDict
-
+import pandas as pd
+from aind_data_schema_models.utils import create_literal_class
 from aind_data_schema_models.pid_names import BaseName
 
 
-class _Registry(BaseName):
+class Registry(BaseName):
     """Base model config"""
 
     model_config = ConfigDict(frozen=True)
 
-
-class Addgene(_Registry):
-    """Addgene"""
-
-    name: Literal["Addgene"] = "Addgene"
-    abbreviation: Literal["ADDGENE"] = "ADDGENE"
+    name: str
+    abbreviation: str
 
 
-class MouseGenomeInformatics(_Registry):
-    """MouseGenomeInformatics"""
-
-    name: Literal["Mouse Genome Informatics"] = "Mouse Genome Informatics"
-    abbreviation: Literal["MGI"] = "MGI"
-
-
-class ResearchOrganizationRegistry(_Registry):
-    """ResearchOrganizationRegistry"""
-
-    name: Literal["Research Organization Registry"] = "Research Organization Registry"
-    abbreviation: Literal["ROR"] = "ROR"
-
-
-class ResearchResourceIdentifiers(_Registry):
-    """ResearchResourceIdentifiers"""
-
-    name: Literal["Research Resource Identifiers"] = "Research Resource Identifiers"
-    abbreviation: Literal["RRID"] = "RRID"
-
-
-class NationalCenterForBiotechnologyInformation(_Registry):
-    """NationalCenterForBiotechnologyInformation"""
-
-    name: Literal["National Center for Biotechnology Information"] = "National Center for Biotechnology Information"
-    abbreviation: Literal["NCBI"] = "NCBI"
-
-
-class OpenResearcherAndContributorID(_Registry):
-    """NationalCenterForBiotechnologyInformation"""
-
-    name: Literal["Open Researcher and Contributor ID"] = "Open Researcher and Contributor ID"
-    abbreviation: Literal["ORCID"] = "ORCID"
-
-
-class Registry:
-    """Registry definitions"""
-
-    ADDGENE = Addgene()
-    ROR = ResearchOrganizationRegistry()
-    MGI = MouseGenomeInformatics()
-    NCBI = NationalCenterForBiotechnologyInformation()
-    ORCID = OpenResearcherAndContributorID()
-    RRID = ResearchResourceIdentifiers()
-
-    _ALL = tuple(_Registry.__subclasses__())
+Registries = create_literal_class(
+    objects=pd.read_csv('models/registries.csv').to_dict(orient='records'), 
+    class_name='Registries', 
+    base_model=Registry, 
+    discriminator='name'
+)
