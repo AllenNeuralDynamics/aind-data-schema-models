8c6faf706e2f2199a839e0f287b2661dfa792130
Dan Birman <danbirman@gmail.com>
2024-08-05 11:10:43 -0700

    feat: creating a basic mouse anatomy model for testing

---
+++ src/aind_data_schema_models/mouse_anatomy_ontology.py
@@ -0,0 +1,23 @@
+"""Module for Mouse Anatomy"""
+
+from importlib_resources import files
+from pydantic import BaseModel, ConfigDict, Field
+
+from aind_data_schema_models.utils import create_literal_class, read_csv
+
+
+class MouseAnatomyModel(BaseModel):
+    """Base model config"""
+
+    model_config = ConfigDict(frozen=True)
+    emapa_id: int = Field(..., title="Structure EMAPA ID")
+    name: str = Field(..., title="Structure name")
+
+
+MouseAnatomyType = create_literal_class(
+    objects=read_csv(str(files("aind_data_schema_models.models").joinpath("mouse_dev_anat_ontology.csv"))),
+    class_name="MouseAnatomyType",
+    base_model=MouseAnatomyModel,
+    discriminator="emapa_id",
+    class_module=__name__,
+)
