6b6364d95d7f35ffe282906d951ee8e37e6bd0e2
Dan Birman <danbirman@gmail.com>
2024-09-13 08:45:24 -0700

    tests: fixing test class name and construction tests

--- tests/test_system_architecture.py
+++ tests/test_system_architecture.py
@@ -4,11 +4,13 @@ import unittest
 from aind_data_schema_models.system_architecture import OperatingSystem, CPUArchitecture
 
 
-class UnitsTests(unittest.TestCase):
+class TestSystemArchitecture(unittest.TestCase):
     """Class for testing Utils.Units"""
 
-    def test_system_architecture(self):
+    def test_class_Construction(self):
         """Tests that OperatingSystem/CPUArchitecture instantiate"""
 
         self.assertIsNotNone(OperatingSystem.MACOS_SONOMA)
         self.assertIsNotNone(CPUArchitecture.X86_64)
+        self.assertEqual(OperatingSystem.DEBIAN_11, OperatingSystem("Debian 11"))
+        self.assertEqual(CPUArchitecture.ARM, CPUArchitecture("Arm32"))
