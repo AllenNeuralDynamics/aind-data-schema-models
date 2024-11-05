0d17ba84bce3bca15559830da0ae38bc0cbc5272
Dan Birman <danbirman@gmail.com>
2024-11-04 18:41:32 -0800

    fix: return missing one_of_instance classes to Organization

--- src/aind_data_schema_models/utils.py
+++ src/aind_data_schema_models/utils.py
@@ -1,6 +1,8 @@
 """ General utilities for constructing models from CSV files """
 
 import re
+from pydantic import BaseModel
+from typing import Annotated, Union, Field, List, Type, Any
 
 
 def to_class_name_underscored(name: str) -> str:
@@ -11,3 +13,25 @@ def to_class_name_underscored(name: str) -> str:
 def to_class_name(name: str) -> str:
     """Convert a name to a valid class name by capitalizing and removing non-alphanumeric characters."""
     return re.sub(r"\W|^(?=\d)", "_", name.title()).replace(" ", "")
+
+
+def one_of_instance(instances: List[Type[BaseModel]], discriminator="name") -> Annotated[Union[Any], Field]:
+    """
+    Make an annotated union of class instances
+    Parameters
+    ----------
+    instances : List[Type[BaseModel]]
+      A list of class instances.
+    discriminator : str
+      Each model in instances should have a common field name where each item
+      is unique to the model. This will allow pydantic to know which class
+      should be deserialized. Default is 'name'.
+
+    Returns
+    -------
+    Annotated[Union[Any], Field]
+      An annotated field that can be used to define a type where a choice from a
+      possible set of classes can be selected.
+
+    """
+    return Annotated[Union[tuple(type(i) for i in instances)], Field(discriminator=discriminator)]
