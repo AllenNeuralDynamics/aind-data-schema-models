d19875212ec686edb4017ebec1570ef82128d365
Dan Birman <danbirman@gmail.com>
2024-11-06 11:01:22 -0800

    feat: convert models to macro templates (#94)
    * feat: convert all models to jinja2 macro templates
    * update tests
    * add coverage

--- src/aind_data_schema_models/registries.py
+++ src/aind_data_schema_models/registries.py
@@ -1,40 +1,88 @@
-"""Common registries"""
+"""Registries"""
 
-from typing import Union
+from typing import Literal, Union
 
-from importlib_resources import files
 from pydantic import ConfigDict, Field
 from typing_extensions import Annotated
 
 from aind_data_schema_models.pid_names import BaseName
-from aind_data_schema_models.utils import create_literal_class, read_csv
 
 
-class RegistryModel(BaseName):
-    """Base model config"""
+class _RegistryModel(BaseName):
+    """Base model for registries"""
 
     model_config = ConfigDict(frozen=True)
-
-    name: str = Field(..., title="Registry name")
-    abbreviation: str = Field(..., title="Registry abbreviation")
-
-
-Registry = create_literal_class(
-    objects=read_csv(str(files("aind_data_schema_models.models").joinpath("registries.csv"))),
-    class_name="Registry",
-    base_model=RegistryModel,
-    discriminator="abbreviation",
-    class_module=__name__,
-)
-
-Registry.abbreviation_map = {m().abbreviation: m() for m in Registry.ALL}
-Registry.from_abbreviation = lambda x: Registry.abbreviation_map.get(x)
+    name: str
+    abbreviation: str
 
 
-def map_registry(abbreviation: str, record: dict, *args):
-    """replace the "registry" key of a dictionary with a RegistryModel object"""
-    registry = Registry.from_abbreviation(abbreviation)
-    if registry:
-        record["registry"] = Annotated[Union[type(registry)], Field(default=registry, discriminator="name")]
-    else:
-        record["registry"] = Annotated[None, Field(None)]
+class _Addgene(_RegistryModel):
+    """Model ADDGENE"""
+
+    name: Literal["Addgene"] = "Addgene"
+    abbreviation: Literal["ADDGENE"] = "ADDGENE"
+
+
+class _Emapa(_RegistryModel):
+    """Model EMAPA"""
+
+    name: Literal["Edinburgh Mouse Atlas Project"] = "Edinburgh Mouse Atlas Project"
+    abbreviation: Literal["EMAPA"] = "EMAPA"
+
+
+class _Mgi(_RegistryModel):
+    """Model MGI"""
+
+    name: Literal["Mouse Genome Informatics"] = "Mouse Genome Informatics"
+    abbreviation: Literal["MGI"] = "MGI"
+
+
+class _Ncbi(_RegistryModel):
+    """Model NCBI"""
+
+    name: Literal["National Center for Biotechnology Information"] = "National Center for Biotechnology Information"
+    abbreviation: Literal["NCBI"] = "NCBI"
+
+
+class _Orcid(_RegistryModel):
+    """Model ORCID"""
+
+    name: Literal["Open Researcher and Contributor ID"] = "Open Researcher and Contributor ID"
+    abbreviation: Literal["ORCID"] = "ORCID"
+
+
+class _Ror(_RegistryModel):
+    """Model ROR"""
+
+    name: Literal["Research Organization Registry"] = "Research Organization Registry"
+    abbreviation: Literal["ROR"] = "ROR"
+
+
+class _Rrid(_RegistryModel):
+    """Model RRID"""
+
+    name: Literal["Research Resource Identifiers"] = "Research Resource Identifiers"
+    abbreviation: Literal["RRID"] = "RRID"
+
+
+class Registry:
+    """Registries"""
+
+    ADDGENE = _Addgene()
+    EMAPA = _Emapa()
+    MGI = _Mgi()
+    NCBI = _Ncbi()
+    ORCID = _Orcid()
+    ROR = _Ror()
+    RRID = _Rrid()
+
+    ALL = tuple(_RegistryModel.__subclasses__())
+
+    ONE_OF = Annotated[Union[tuple(_RegistryModel.__subclasses__())], Field(discriminator="abbreviation")]
+
+    abbreviation_map = {m().abbreviation: m() for m in ALL}
+
+    @classmethod
+    def from_abbreviation(cls, abbreviation: str):
+        """Get registry from abbreviation"""
+        return cls.abbreviation_map.get(abbreviation, None)
