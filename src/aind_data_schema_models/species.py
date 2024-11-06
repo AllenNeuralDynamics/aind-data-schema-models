ca093fa98536d4b37c50198ff270762991070856
Dan Birman <danbirman@gmail.com>
2024-11-05 23:12:04 -0800

    chore: lint

--- src/aind_data_schema_models/species.py
+++ src/aind_data_schema_models/species.py
@@ -5,7 +5,7 @@ from typing import Literal, Union
 from pydantic import BaseModel, ConfigDict, Field
 from typing_extensions import Annotated
 
-from aind_data_schema_models.registries import Registry, _RegistryModel
+from aind_data_schema_models.registries import Registry
 
 
 class _SpeciesModel(BaseModel):
