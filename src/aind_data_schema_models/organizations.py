1129b51a370bf9211ed2d7dcc4cf3807b209c994
Saskia de Vries <sejdevries@gmail.com>
2024-12-17 14:27:07 -0800

    moved to template

--- src/aind_data_schema_models/organizations.py
+++ src/aind_data_schema_models/organizations.py
@@ -1125,12 +1125,3 @@ Organization.SUBJECT_SOURCES = one_of_instance(
         Organization.OTHER,
     ]
 )
-
-Organization.CATHETER_IMPLANTS = one_of_instance(
-    [
-        Organization.AIND,
-        Organization.CRL,
-        Organization.JAX,
-        Organization.OTHER,
-    ]
-)
