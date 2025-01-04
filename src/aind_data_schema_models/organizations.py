c27cfb26ee097751df753224f90f2f24c8b24526
Saskia de Vries <sejdevries@gmail.com>
2025-01-03 16:31:32 -0800

    forgot to run

--- src/aind_data_schema_models/organizations.py
+++ src/aind_data_schema_models/organizations.py
@@ -1042,6 +1042,7 @@ Organization.LENS_MANUFACTURERS = one_of_instance(
         Organization.FUJINON,
         Organization.HAMAMATSU,
         Organization.INFINITY_PHOTO_OPTICAL,
+        Organization.KOWA,
         Organization.LEICA,
         Organization.MITUTUYO,
         Organization.NAVITAR,
@@ -1086,7 +1087,9 @@ Organization.MANIPULATOR_MANUFACTURERS = one_of_instance([Organization.NEW_SCALE
 
 Organization.MONITOR_MANUFACTURERS = one_of_instance([Organization.ASUS, Organization.LG, Organization.OTHER])
 
-Organization.SPEAKER_MANUFACTURERS = one_of_instance([Organization.TYMPHANY, Organization.ISL, Organization.OTHER])
+Organization.SPEAKER_MANUFACTURERS = one_of_instance(
+    [Organization.DIGIKEY, Organization.TYMPHANY, Organization.ISL, Organization.OTHER]
+)
 
 Organization.FUNDERS = one_of_instance(
     [
