1070b430719055e332bb983e4a06669e8a978b21
Helen Lin <46795546+helen-m-lin@users.noreply.github.com>
2025-12-03 15:50:50 -0800

    ci: update github action to publish models to docdb (#256)
    * ci: update aind-data-access-api dependency for REST client
    
    * refactor: update write_to_docdb.py script to use docdb rest api
    
    * feat: add _id, error handling, logging
    
    * feat: write each file as single record to 1 collection
    
    * feat: delete records in docdb that do not have a csv file
    
    * ci: update GitHub actions - publish workflows
    
    * fix: remove extra space

--- scripts/write_to_docdb.py
+++ scripts/write_to_docdb.py
@@ -1,17 +1,17 @@
 """Module to publish models to docdb"""
 
-import os
 import csv
-from typing import Iterator
-from aind_data_access_api.document_db_ssh import (
-    DocumentDbSSHClient,
-    DocumentDbSSHCredentials,
-)
+import os
+from typing import Iterator, List
 
-DB_NAME = os.getenv("DB_NAME")
+from aind_data_access_api.document_db import Client as DocDBClient
+from requests import HTTPError
+
+DOCDB_HOST = os.getenv("DOCDB_HOST")
+DOCDB_DATABASE = os.getenv("DOCDB_DATABASE")
+DOCDB_COLLECTION = os.getenv("DOCDB_COLLECTION")
+
 PATH_TO_MODELS = os.getenv("PATH_TO_MODELS")
-DOCDB_READWRITE_SECRET = os.getenv("DOCDB_READWRITE_SECRET")
-DOCDB_SSH_TUNNEL_SECRET = os.getenv("DOCDB_SSH_TUNNEL_SECRET")
 AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")
 
 
@@ -25,28 +25,64 @@ def csv_to_json(csv_file_path: str) -> Iterator:
             yield row
 
 
-def publish_to_docdb(folder_path: str, credentials: DocumentDbSSHCredentials) -> None:
+def get_csv_paths(folder_path: str) -> List[str]:
+    """Parse a folder and return a list of csv file paths"""
+    file_paths = list()
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith(".csv"):
+            file_paths.append(os.path.join(folder_path, file_name))
+    return file_paths
+
+
+def publish_to_docdb(file_paths: List[str], docdb_client: DocDBClient) -> None:
     """
-    Writes models to docdb
+    Writes each csv file as one record in docdb. Assumes file size does not
+    exceed API gateway and DocDB limits.
     """
-    with DocumentDbSSHClient(credentials=credentials) as doc_db_client:
-        database = doc_db_client._client[doc_db_client.database_name]
-        for file_name in os.listdir(folder_path):
-            if file_name.endswith(".csv"):
-                collection_name = file_name[:-4]
-                collection = database[collection_name]
-                csv_file_path = os.path.join(folder_path, file_name)
-                json_data = csv_to_json(csv_file_path)
-                for records in json_data:
-                    filter = {"name": records["name"]}
-                    response = collection.update_one(filter=filter, update={"$set": records}, upsert=True)
-                    print(response.raw_result)
+    for file_path in file_paths:
+        file_name = os.path.basename(file_path)
+        print(f"Processing file: {file_name}")
+        csv_contents = list(csv_to_json(file_path))
+        record = {
+            "_id": file_name.removesuffix(".csv"),
+            "file_name": file_name,
+            "count": len(csv_contents),
+            "contents": csv_contents,
+        }
+        print(f"Upserting {file_name} contents to docdb")
+        response = docdb_client.upsert_one_docdb_record(record=record)
+        print(response.json())
+
+
+def remove_unmatched_records(file_paths: List[str], docdb_client: DocDBClient) -> None:
+    """
+    Deletes records in docdb that do not correspond to any csv files.
+    """
+    ids_from_csvs = {os.path.basename(fp).removesuffix(".csv") for fp in file_paths}
+
+    records = docdb_client.retrieve_docdb_records(projection={"_id": 1})
+    ids_from_docdb = {record["_id"] for record in records}
+
+    ids_to_delete = ids_from_docdb - ids_from_csvs
+    print(f"Ids in DocDB with no corresponding CSV file: {ids_to_delete}")
+    if ids_to_delete:
+        print(f"Deleting {len(ids_to_delete)} records from DocDB")
+        response = docdb_client.delete_many_records(data_asset_record_ids=list(ids_to_delete))
+        print(response.json())
 
 
 if __name__ == "__main__":
-    folder_path = PATH_TO_MODELS
-    credentials = DocumentDbSSHCredentials.from_secrets_manager(
-        doc_db_secret_name=DOCDB_READWRITE_SECRET, ssh_secret_name=DOCDB_SSH_TUNNEL_SECRET
+    file_paths = get_csv_paths(PATH_TO_MODELS)
+    print(f"Found {len(file_paths)} csv files to process")
+
+    docdb_client = DocDBClient(
+        host=DOCDB_HOST,
+        database=DOCDB_DATABASE,
+        collection=DOCDB_COLLECTION,
     )
-    credentials.database = DB_NAME
-    publish_to_docdb(folder_path, credentials)
+    try:
+        publish_to_docdb(file_paths=file_paths, docdb_client=docdb_client)
+        remove_unmatched_records(file_paths=file_paths, docdb_client=docdb_client)
+    except HTTPError as error:
+        print(f"HTTP error {error.response.status_code}: {error.response.text}")
+        raise error
