4cbeb5320481ad92ce1791d7f094f4d23a432b5a
Dan Birman <danbirman@gmail.com>
2026-03-22 07:57:29 -0700

    fix: remove group tests

--- tests/test_organizations.py
+++ tests/test_organizations.py
@@ -24,15 +24,5 @@ class TestOrganization(unittest.TestCase):
 
         self.assertEqual(Organization.from_abbreviation(None), None)
 
-    def test_groups(self):
-        """Test that the organization groups are present"""
-
-        union_types = get_args(Organization.SUBJECT_SOURCES.__origin__)
-        self.assertTrue(any(isinstance(Organization.AI, t) for t in union_types))
-
-        union_types = get_args(Organization.RESEARCH_INSTITUTIONS.__origin__)
-        self.assertTrue(any(isinstance(Organization.AIBS, t) for t in union_types))
-
-
 if __name__ == "__main__":
     unittest.main()
