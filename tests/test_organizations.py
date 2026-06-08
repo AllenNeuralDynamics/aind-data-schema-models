25a7ad8bd9f3de498f956502e730779022eb521c
Dan Birman <danbirman@gmail.com>
2026-06-08 10:06:16 -0700

    BREAKING CHANGE: Remove sublists in Organization (#286)

--- tests/test_organizations.py
+++ tests/test_organizations.py
@@ -3,7 +3,6 @@
 import unittest
 
 from aind_data_schema_models.organizations import Organization
-from typing import get_args
 
 
 class TestOrganization(unittest.TestCase):
@@ -23,15 +22,6 @@ class TestOrganization(unittest.TestCase):
         """Test that you can't get an organization from None"""
 
         self.assertEqual(Organization.from_abbreviation(None), None)
-
-    def test_groups(self):
-        """Test that the organization groups are present"""
-
-        union_types = get_args(Organization.SUBJECT_SOURCES.__origin__)
-        self.assertTrue(any(isinstance(Organization.AI, t) for t in union_types))
-
-        union_types = get_args(Organization.RESEARCH_INSTITUTIONS.__origin__)
-        self.assertTrue(any(isinstance(Organization.AIBS, t) for t in union_types))
 
 
 if __name__ == "__main__":
